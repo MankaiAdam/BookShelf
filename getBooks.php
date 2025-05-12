5e618fb5ec870221e1904b028db08c60a17e8110
MankaiAdam <mankai.adam@gmail.com>
2025-05-12 19:09:34 +0100

    styles

--- getBooks.php
+++ getBooks.php
@@ -13,14 +13,12 @@ while($row = $result->fetch_assoc()){
     $books[] = $row;
 }
 $conn->close();
-
 ?>
 
-<!-- HTML Part: always show the form -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=1.0">
     <title>Books</title>
 </head>
 <body>
--- getUsers.php
+++ getUsers.php
@@ -16,11 +16,10 @@ while($row = $result->fetch_assoc()){
 $conn->close();
 ?>
 
-<!-- HTML Part: always show the form -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=1.0">
     <title>Users</title>
 </head>
 <body>
